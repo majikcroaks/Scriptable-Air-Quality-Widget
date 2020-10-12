796ac14ec85989bef5f4885403a68b9ea07d73d9
Jimi <z44x85r@hotmail.com>
2020-10-13 00:16:57 +0800

    add color change

--- AirBox.js
+++ AirBox.js
@@ -3,9 +3,26 @@
 // icon-color: deep-green; icon-glyph: leaf;
 // Based on code by Jason Snell <jsnell@sixcolors.com>, Matt Silverlock
 // Inspired and based on Apple Shortcuts by jickey@PTT-iOS
-// API powered by Powered by Location Aware Sensing System (LASS) and IIS-NRL, Academia Sinica 
+// API powered by Location Aware Sensing System (LASS) and IIS-NRL, Academia Sinica 
 
 const API_URL = "https://pm25.lass-net.org/API-1.0.0/";
+
+// load and validate spotify credentials from iCloud Drive
+async function loadSensor() {
+    let fm = FileManager.iCloud();
+    let dir = fm.documentsDirectory();
+    let path = fm.joinPath(dir, "sensor.json");
+    let sensor;
+    if (fm.fileExists(path)) {
+        await fm.downloadFileFromiCloud(path);
+        let sensorFile = Data.fromFile(path);
+        sensor = JSON.parse(sensorFile.toRawString());
+        if (isNotEmpty(sensor.device_id)) {
+            return sensor.device_id;
+        }
+    }
+    return null;
+}
 
 async function getSensorList() {
     //Get coordinates.
@@ -29,14 +46,14 @@ async function getSensorList() {
     }
     //Return first sensor, which is the nearest one.
     return ID[0];
-};
+}
 
 //Get sensor data.
 async function getSensorData() {
-    //Get nearest sensor ID from getSensorList().
-    let device_id = await getSensorList();
+    //Load saved sensor or get nearest sensor ID from getSensorList().
+    let device_id = await loadSensor() || await getSensorList();
     console.log(`Using sensor: ${device_id}`);
-    let sensorData = new Request(`https://pm25.lass-net.org/API-1.0.0/device/${device_id}/latest/?format=JSON`);
+    let sensorData = new Request(`https://pm25.lass-net.org/data/last.php?device_id=${device_id}`);
     let raw = await sensorData.loadJSON();
     //Parse JSON.
     let data = raw.feeds[0].AirBox;
@@ -47,10 +64,8 @@ async function getSensorData() {
         pm25: data.s_d0,
         name: data.SiteName,
         timestamp: data.timestamp
-    }
+    };
 }
-
-
 
 //Mapping the PM 2.5 levels with corresponding labels.
 function getLevel(pm25) {
@@ -60,37 +75,55 @@ function getLevel(pm25) {
         startColor: "white",
         endColor: "white",
         textColor: "black",
-    }
+    };
     if (pm25 <= 15) {
         return {
             threshold: 15,
-            label: "良好"
-        }
+            label: "良好",
+            startColor: "00e400",
+            endColor: "00bb00",
+            textColor: "000000",
+        };
     } else if (pm25 <= 35) {
         return {
             threshold: 35,
-            label: "普通"
-        }
+            label: "普通",
+            startColor: "ffff00",
+            endColor: "cccc00",
+            textColor: "000000",
+        };
     } else if (pm25 <= 54) {
         return {
             threshold: 54,
-            label: "對敏感族群不健康"
-        }
+            label: "對敏感族群不健康",
+            startColor: "FFA63D",
+            endColor: "D67200",
+            textColor: "000000",
+        };
     } else if (pm25 <= 150) {
         return {
             threshold: 150,
-            label: "不健康"
-        }
+            label: "不健康",
+            startColor: "FF3D3D",
+            endColor: "D60000",
+            textColor: "000000",
+        };
     } else if (pm25 <= 250) {
         return {
             threshold: 250,
-            label: "非常不健康"
-        }
+            label: "非常不健康",
+            startColor: "8f3f97",
+            endColor: "6f1f77",
+            textColor: "ffffff",
+        };
     } else if (pm25 <= 999 || pm25 > 999) {
         return {
             threshold: 999,
-            label: "有害"
-        }
+            label: "有害",
+            startColor: "9e2043",
+            endColor: "7e0023",
+            textColor: "ffffff",
+        };
     }
     return result;
 }
@@ -110,15 +143,15 @@ function getLevel(pm25) {
         let textColor = '112A46';
         console.log(level);
 
-        //let startColor = new Color(level.startColor);
-        //let endColor = new Color(level.endColor);
-        //let gradient = new LinearGradient({
-        //    colors: [startColor, endColor],
-        //    locations: [0, 1]
-        //});
-        //console.log(gradient);
+        let startColor = new Color(level.startColor);
+        let endColor = new Color(level.endColor);
+        let gradient = new LinearGradient({
+            colors: [startColor, endColor],
+            locations: [0, 1]
+        });
+        console.log(gradient);
 
-        //wg.backgoundGradient = gradient;
+        wg.backgoundGradient = gradient;
         wg.backgroundColor = new Color('F7F7F7');
 
         let header = wg.addText(`${level.label}`);
@@ -129,10 +162,10 @@ function getLevel(pm25) {
 
         let content = wg.addText(`粉塵 ${pm25} ug/m3`);
         content.textColor = new Color(textColor);
-        content.font = Font.regularSystemFont(10);
+        content.font = Font.regularSystemFont(12);
 
 
-        let wordTemp = wg.addText(`${temp}°C`);
+        let wordTemp = wg.addText(`${temp} °C`);
         wordTemp.textColor = new Color(textColor);
         wordTemp.font = Font.regularSystemFont(12);
 
